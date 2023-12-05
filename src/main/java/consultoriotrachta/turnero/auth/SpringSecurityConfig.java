1b14c1620a1a1bede7df4f9c626cf44f140c3a51
mjtrachta <61702195+mjtrachta@users.noreply.github.com>
2023-12-05 06:37:36 -0300

    cors

--- src/main/java/consultoriotrachta/turnero/auth/SpringSecurityConfig.java
+++ src/main/java/consultoriotrachta/turnero/auth/SpringSecurityConfig.java
@@ -2,9 +2,12 @@ package consultoriotrachta.turnero.auth;
 
 import consultoriotrachta.turnero.auth.filters.JwtAuthenticationFilter;
 import consultoriotrachta.turnero.auth.filters.JwtValidationFilter;
+import jakarta.servlet.FilterRegistration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -15,6 +18,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @Configuration
 public class SpringSecurityConfig {
@@ -44,6 +53,31 @@ public class SpringSecurityConfig {
                 .addFilter(new JwtValidationFilter(authenticationConfiguration.getAuthenticationManager()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(managment -> managment.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .cors(cors ->cors.configurationSource(corsConfigurationSource()))
                 .build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration config =  new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT"));
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+
+    @Bean
+    FilterRegistrationBean<CorsFilter> corsFilter(){
+        FilterRegistrationBean<CorsFilter> bean  = new FilterRegistrationBean<>(new CorsFilter(
+                corsConfigurationSource()));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+         return  bean;
+
     }
 }
--- src/main/java/consultoriotrachta/turnero/auth/TokenJwtConfig.java
+++ src/main/java/consultoriotrachta/turnero/auth/TokenJwtConfig.java
@@ -9,7 +9,7 @@ import java.security.Key;
 
 public class TokenJwtConfig {
 
-    //public final static String SECRET_KEY = "algun_tokem_con_alguna_frase_secreta";
+
     public final static SecretKey SECRET_KEY = Jwts.SIG.HS512.key().build();
     public final static String PREFIX_TOKEN = "Bearer ";
     public final static String HEADER_AUTHORIZATION = "authorization";
